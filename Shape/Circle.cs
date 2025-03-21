456a6b4def2e95b01d27edac1fc4b42c80d5ea64
Petrik Diak <diak@petrik.hu>
2025-03-21 12:40:32 +0100

    qwefsf

---
+++ Shape/Circle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape
+{
+	internal class Circle : Shape
+	{
+
+		double r;
+		public Circle(bool isHoley, double r, string color) : base(isHoley, color)
+		{
+			this.r = r;
+		}
+
+		public double R { get => r; set => r = value; }
+
+		public override double Area()
+		{
+			return Math.Pow(r, 2) * Math.PI;
+		}
+
+		public override double Perimater()
+		{
+			return 2 * r * Math.PI;
+		}
+
+		public override string? ToString()
+		{
+			return base.ToString() + " - Circle";
+		}
+	}
+}
--- Shape/Program.cs
+++ Shape/Program.cs
@@ -4,7 +4,18 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
-		}
+			Rectangle rectangle = new(false, "red", 1, 2);
+
+            Console.WriteLine(rectangle.ToString());
+
+			Shape[] shapes =
+			{
+				rectangle,
+				new Square(true, "blue", 1, 1),
+				new Circle(false, 5, "black")
+			};
+
+
+        }
 	}
 }
